09b3982c549dc0aeafeac747203f14ba62e6debd
banysk <banysk@users.noreply.github.com>
2022-06-18 15:26:43 +1000

    Добавьте файлы проекта.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class UserController : Controller
+    {
+        #region Страница пользователя
+        /// <summary>
+        /// Возвращает страницу с данными пользователя
+        /// </summary>
+        [HttpGet]
+        [Route("User/{user_name}")]
+        public IActionResult UserPage()
+        {
+            return View("UserPage");
+        }
+        #endregion
+
+        #region Авторизация пользователя
+        /// <summary>
+        /// Возвращает страницу для авторизации пользователя
+        /// </summary>
+        [HttpGet]
+        [Route("Login")]
+        [Route("User/Login")]
+        public IActionResult LoginPage()
+        {
+            return View("LoginPage");
+        }
+        #endregion
+
+        #region Регистрация пользователя
+        /// <summary>
+        /// Возвращает страницу для регистрации пользователя
+        /// </summary>
+        [HttpGet]
+        [Route("")]
+        [Route("Registration")]
+        [Route("User/Registration")]
+        public IActionResult RegistrationPage()
+        {
+            return View("RegistrationPage");
+        }
+        #endregion
+    }
+}
